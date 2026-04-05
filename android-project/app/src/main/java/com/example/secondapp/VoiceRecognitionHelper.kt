977a2d1066c9b97626be14e0bd57fbf2a5e20237
Daxxsc <1121649724@qq.com>
2026-04-05 21:18:43 +0800

    fix: 修复 Intent 和 Bundle 导入问题

--- android-project/app/src/main/java/com/example/secondapp/VoiceRecognitionHelper.kt
+++ android-project/app/src/main/java/com/example/secondapp/VoiceRecognitionHelper.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
